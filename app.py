495d3e16b91432fac2878dbabd68d9c7f4139938
lintivities <lindsayndungu44@gmail.com>
2025-03-04 10:41:33 +0300

    Updated text formatting

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello ():
-    return "Hello MR Oloo! This is Lindsay"
+def hello():
+    return "<h1><b>Hello,  This is Lindsay Wangari</b></h1>"
+
 if __name__ == '__main__':
     app.run(debug=True)
